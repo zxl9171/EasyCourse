8eb9c8c516acc167a37d055dcf722c36a8fdf4b6
stranbird <meteey@gmail.com>
2012-11-15 23:47:23 +0800

    BUG - minor fix

--- docs/renderer/Renderer.rb
+++ docs/renderer/Renderer.rb
@@ -79,22 +79,18 @@ def html2pdf(options)
         "page-bottom"=> 70
     }.merge(options).stringify_keys!
 
-    if opts['output_file'].nil? then
-        opts['output_file'] = corename(opts['input_file'])
-    end
-
     opts_string = opts.reject {|e| e[0] == "input_file" }.inject("") {|str, h| str += " -o #{h[0]}=#{h[1]} " }
 
-    `cupsfilter #{opts_string} #{opts['input_file']} > #{opts['output_file']}`
+    `cupsfilter #{opts_string} #{opts['input_file']} > #{opts['output_file']} 2> /dev/null`
 end
 
 
-if ARGV.length <= 2 then
+if ARGV.length <= 1 then
     puts 'USAGE: ruby Renderer input_file output_file template_file'
 else
     input_file =ARGV[0]
     output_file = ARGV[1]
-    frontpage_file = ARGV[2]
+    frontpage_file = ARGV[2] || same_dir_path('frontpage.html')
 
     o = MDParser.new(input_file)
     r = Renderer.load_template(frontpage_file, o.meta)
@@ -113,11 +109,13 @@ else
         html_tmpfile.close
 
         md2html({content: content_tmpfile.path, frontpage: frontpage_tmpfile.path, output_file: html_tmpfile.path})
+        puts "md2html done!"
 
-        puts html2pdf({input_file: html_tmpfile.path, output_file: output_file})
+        html2pdf({input_file: html_tmpfile.path, output_file: output_file})
+        puts "html2pdf done! Generated #{output_file}"
     rescue => ex
         puts ex
     ensure
-        # [content_tmpfile, frontpage_tmpfile, html_tmpfile].each(&:unlink)
+        [content_tmpfile, frontpage_tmpfile, html_tmpfile].each(&:unlink)
     end
 end
