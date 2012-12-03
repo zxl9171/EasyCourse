8b176316d61548951299615dc9f75bd6c0569995
stranbird <meteey@gmail.com>
2012-12-04 00:32:56 +0800

    修改了文件列表的样式，并将之模块化

--- src-rails/app/controllers/dashboard_controller.rb
+++ src-rails/app/controllers/dashboard_controller.rb
@@ -2,12 +2,18 @@
 class DashboardController < ApplicationController
   def index
     notif = {
-              :avatar => '/assets/thumb_3f959077cac64c7b45793c743e22b7cc.jpg',
-              :name => '小明',
-              :title => '我们要不然就抄这个网站好了？',
-              :content => 'R.T',
-              :created_at => '刚刚'
-            }
+      :avatar => '/assets/thumb_3f959077cac64c7b45793c743e22b7cc.jpg',
+      :name => '小明',
+      :title => '我们要不然就抄这个网站好了？',
+      :content => 'R.T',
+      :created_at => '刚刚'
+    }
     @notifs = [notif, notif]
+
+    file = {
+      title: "Snip20121203_2",
+      url: "/assets/Snip20121203_2.png"
+    }
+    @files = [file] * 6
   end
 end
