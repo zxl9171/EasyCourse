aaab4ff2a930d73c3d3d7224b5042f0b47ca8f5d
stranbird <meteey@gmail.com>
2012-12-04 11:03:05 +0800

    新增文件列表view

--- src-rails/app/controllers/files_controller.rb
+++ src-rails/app/controllers/files_controller.rb
@@ -1,9 +1,17 @@
+require 'faker'
+
 class FilesController < ApplicationController
   def index
+    @files = 1.upto(6).map {|e| gen_file }
+  end
+
+private
+  def gen_file
     file = {
-      title: "Snip20121203_2",
-      url: "/assets/Snip20121203_2.png"
+      title: Faker::Lorem.sentence,
+      user_name: Faker::Name.first_name,
+      url: "/assets/Snip20121203_2.png",
+      created_at: rand(10).to_i.day.ago
     }
-    @files = [file] * 6
   end
 end
--- src-rails/config/routes.rb
+++ src-rails/config/routes.rb
@@ -1,6 +1,4 @@
 Easycourse::Application.routes.draw do
-  get "files/index"
-
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
