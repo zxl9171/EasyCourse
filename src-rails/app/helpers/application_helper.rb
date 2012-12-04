442b5c5d8b27eaf6f3e90586bd64f8fbce1086ee
stranbird <meteey@gmail.com>
2012-12-04 11:28:24 +0800

    增加file/show页面，增强了section_header_helper的可定制性

--- src-rails/app/helpers/application_helper.rb
+++ src-rails/app/helpers/application_helper.rb
@@ -3,8 +3,8 @@ module ApplicationHelper
     render :partial => "misc/header", :locals => { :header => header }
   end
 
-  def render_section_header(header, action_tip = nil, icon = nil)
-    render :partial => "misc/section_header", :locals => { :section_header => header, :action_tip => action_tip, :icon => icon }
+  def render_section_header(header, action_tip = nil, icon = nil, options = nil)
+    render :partial => "misc/section_header", :locals => { :section_header => header, :action_tip => action_tip, :icon => icon, :options => options }
   end
 
   def render_view_more(text = '', url = '')
