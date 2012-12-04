21aff40fd33228560ee1b2c9d0f7c7ce47881a4f
stranbird <meteey@gmail.com>
2012-12-04 10:22:18 +0800

    重构了view，把共有模块提取到了misc，然后用helper来提供渲染接口

--- src-rails/app/controllers/files_controller.rb
+++ src-rails/app/controllers/files_controller.rb
@@ -1,4 +1,9 @@
 class FilesController < ApplicationController
   def index
+    file = {
+      title: "Snip20121203_2",
+      url: "/assets/Snip20121203_2.png"
+    }
+    @files = [file] * 6
   end
 end
--- src-rails/app/helpers/application_helper.rb
+++ src-rails/app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def render_header(header)
+    render :partial => "misc/header", :locals => { :header => header }
+  end
+
+  def render_section_header(header, action_tip = nil, icon = nil)
+    render :partial => "misc/section_header", :locals => { :section_header => header, :action_tip => action_tip, :icon => icon }
+  end
+
+  def render_view_more(text = '', url = '')
+    render :partial => "misc/view_more", :locals => { text: text, url: url }
+  end
 end
