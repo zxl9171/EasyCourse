36c131c0e810b479776aa04be8117a989acd28da
stranbird <meteey@gmail.com>
2012-12-04 11:51:04 +0800

    修个小bug

--- src-rails/app/helpers/application_helper.rb
+++ src-rails/app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
     render :partial => "misc/header", :locals => { :header => header }
   end
 
-  def render_section_header(header, action_tip = nil, icon = nil, options = nil)
+  def render_section_header(header, action_tip = nil, icon = nil, options = {})
     render :partial => "misc/section_header", :locals => { :section_header => header, :action_tip => action_tip, :icon => icon, :options => options }
   end
 
