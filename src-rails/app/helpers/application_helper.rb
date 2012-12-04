bcfc3a80dfdeb57267f3e4dbbd9101daa33e0198
stranbird <meteey@gmail.com>
2012-12-04 18:04:10 +0800

    加入comment form

--- src-rails/app/helpers/application_helper.rb
+++ src-rails/app/helpers/application_helper.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 module ApplicationHelper
   def render_header(header)
     render :partial => "misc/header", :locals => { :header => header }
@@ -9,5 +10,12 @@ module ApplicationHelper
 
   def render_view_more(text = '', url = '')
     render :partial => "misc/view_more", :locals => { text: text, url: url }
+  end
+
+  def current_user
+    {
+      name: '小明',
+      avatar: '/assets/thumb_3f959077cac64c7b45793c743e22b7cc.jpg'
+    }
   end
 end
